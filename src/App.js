c70ef5f7311c6717d2a2628b0af82ff0af2169a7
jkkjkhh <jkkjkhh@gmail.com>
2022-09-09 20:32:18 +0900

    useState를 활용한 글 리스트 생성

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  let post = "강남 우동 맛집";
+  let [글제목, b] = useState([
+    "강남 우동 맛집",
+    "남자 코트 추천",
+    "여자 코트 추천",
+  ]);
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{ color: "white", fontSize: "20px" }}>블로그</h4>
+        <h4>블로그</h4>
       </div>
-      <h4>{post}</h4>
+      <div className="list">
+        <h4>{글제목[0]}</h4>
+        <p>9월 9일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>9월 10일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
+        <p>9월 11일 발행</p>
+      </div>
     </div>
   );
 }
