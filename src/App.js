a8d9ac6f75a65ce8a57b51aa0458f57e808a0540
jkkjkhh <jkkjkhh@gmail.com>
2022-09-13 13:51:00 +0900

    map을 활용한 리스트 및 개별 좋아요 추가

--- src/App.js
+++ src/App.js
@@ -1,30 +1,85 @@
+// /* eslint-disable */ // warning 메시지 없애주는거
 import "./App.css";
 import { useState } from "react";
 
 function App() {
-  let [글제목, b] = useState([
-    "강남 우동 맛집",
+  let [글제목, set글제목] = useState([
     "남자 코트 추천",
-    "여자 코트 추천",
+    "강남 우동 맛집",
+    "파이썬 독학",
   ]);
+  let [따봉, set따봉] = useState([0, 0, 0]);
+
+  let [modal, setModal] = useState(false);
+
+  function 글제목변경() {
+    let copy = [...글제목];
+    copy[0] = "여자 코트 추천";
+    set글제목(copy);
+  }
+
+  function 오름차순정렬() {
+    let copy = [...글제목];
+    copy.sort();
+    set글제목(copy);
+  }
+
+  function modalDetail() {
+    setModal(!modal);
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>블로그</h4>
       </div>
-      <div className="list">
-        <h4>{글제목[0]}</h4>
+      <button onClick={글제목변경}>제목 변경</button>
+
+      <button onClick={오름차순정렬}>가나다순 정렬</button>
+
+      {/* <div className="list">
+        <h4>
+          {글제목[0]}
+          <sapn onClick={플러스함수}>👍</sapn> {따봉}
+        </h4>
+
         <p>9월 9일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{글제목[1]}</h4>
-        <p>9월 10일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{글제목[2]}</h4>
+      </div> */}
+
+      {/* <div className="list">
+        <h4 onClick={modalDetail}>{글제목[2]}</h4>
         <p>9월 11일 발행</p>
-      </div>
+      </div> */}
+
+      {글제목.map(function (a, i) {
+        return (
+          <div className="list" key={i}>
+            <h4 onClick={modalDetail}>{글제목[i]}</h4>
+            <p
+              onClick={() => {
+                let copy = [...따봉];
+                copy[i] = copy[i] + 1;
+                set따봉(copy);
+              }}
+            >
+              👍 {따봉[i]}
+            </p>
+            <p>9월 10일 발행</p>
+          </div>
+        );
+      })}
+
+      {modal === true ? <Modal /> : null}
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
